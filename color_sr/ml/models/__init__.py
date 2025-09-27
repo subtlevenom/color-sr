260fac7a0fb2598d72794abc26f9b1f503dd0905
subtlevenom <andrew.korepanov@gmail.com>
2025-09-27 14:49:12 +0200

    refactoring

--- color_sr/ml/models/__init__.py
+++ color_sr/ml/models/__init__.py
@@ -1,1 +1,14 @@
-from .gsr_kan import GSRKanModel, UnetEncoder, DETrEncoder, ConvEncoder, CMEncoder, KANHead
+import sys
+from openeye.ml.layers.fusion import FusionTR, FusionFFN
+from openeye.ml.layers.common import Flip
+from .gazenet.encoders import DETREncoder, TimmEncoder, UnetEncoder
+from .gazenet.heads import DETRHead, TRHead, KANHead, SimpleHead, GazeHead
+from .meta import Meta
+
+
+def create_model(name, params):
+    pkg = sys.modules[__name__]
+    cls = getattr(pkg, name)
+    if params is None:
+        return cls()
+    return cls(**params)
